c19e5591d41397c6083fefe8a5edd28c7275c605
John Nicholas <john.nicholas@dinoroar.io>
2022-03-27 16:00:26 +0100

    added some docs

--- genetic/lib/problem.ex
+++ genetic/lib/problem.ex
@@ -1,7 +1,22 @@
 defmodule Problem do
   alias Types.Chromosome
 
+  @moduledoc """
+  The basic functions for defining a problem in a GA
+  """
+
+  @doc """
+  returns a Types.Chromasome with an initial set of genes
+  """
   @callback genotype :: Chromosome.t()
+
+  @doc """
+  Given a Types.Chromasome returns a number() that respresents its fitness for the terminate?/1 to reason about
+  """
   @callback fitness_function(Chromosome.t()) :: number()
+
+  @doc """
+  given a population, takes the best (head usually) and establishes if its fitness reaches some predicate
+  """
   @callback terminate?(Enum.t()) :: boolean()
 end
